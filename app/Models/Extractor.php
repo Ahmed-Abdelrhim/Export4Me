af9452ccdea50046b27f4ead6b1b8023d7875109
Ahmed-Abdelrhim <aabdelrhim974@gmail.com>
2023-10-29 11:00:32 +0200

    Adding Fillables Array Inside The Extractor Model

--- app/Models/Extractor.php
+++ app/Models/Extractor.php
@@ -8,4 +8,11 @@ use Illuminate\Database\Eloquent\Model;
 class Extractor extends Model
 {
     use HasFactory;
+
+    protected $table = 'extractors';
+    protected $fillable = ['company_name', 'owner_name', 'manager_name', 'address',
+        'website','media','email','landline','mobile_phone',
+        'shipping_types','harbor_type','clearance_type',
+        'commercial_record','license','created_at','updated_at',
+    ];
 }
