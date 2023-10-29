b212a232eebaa7d78021f7d274b477c511e9a9a8
Ahmed-Abdelrhim <aabdelrhim974@gmail.com>
2023-10-29 10:52:51 +0200

    Adding Extractor Migration

---
+++ app/Http/Controllers/ExtractorController.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ExtractorController extends Controller
+{
+    //
+}
---
+++ database/migrations/2023_10_29_113204_create_extractors_table.php
@@ -0,0 +1,55 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('extractors', function (Blueprint $table) {
+            $table->id();
+
+
+            $table->string('company_name');
+            $table->string('owner_name');
+            $table->string('manager_name');
+            $table->string('address');
+
+            $table->string('website')->nullable();
+            $table->string('media')->nullable();
+
+
+            $table->string('email')->nullable();
+
+
+            $table->string('landline')->nullable();
+            $table->string('mobile_phone')->nullable();
+
+
+
+            $table->text('shipping_types'); // أنواع الشحن
+            $table->string('harbor_type'); //  export Or import (صادر أو وارد)
+            $table->string('clearance_type'); // Marine or aerial or Wild  (بري أو جوي أو بري)
+
+            // images
+            $table->string('commercial_record')->nullable();
+            $table->string('license')->nullable();
+
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('extractors');
+    }
+};
